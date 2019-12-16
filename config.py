90221f158cfb7946a3e17a21a740f0afa8500ef6
Stefano <srensi@stanford.edu>
2019-12-16 12:24:26 -0800

    Delete config.py

--- config.py
+++
@@ -1,2 +0,0 @@
-pocket_feature_url = '/Users/srensi/Documents/GitHub/scaffold_hops/data/pocket_feature_scores.csv'
-ligand_url = '/Users/srensi/Documents/GitHub/scaffold_hops/data/ligand_comparisons.csv'
