7b728ca0545cbce7f9981f7374039b10169bfbe7
Alli Keys <allikeys@Allis-MacBook-Pro-2.local>
2020-04-09 13:46:06 -0700

    data

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-ligand_url = ''
-pocket_feature_url = ''
+ligand_url = '/Users/allikeys/Documents/github/scaffold_hops/data/pocket_feature_scores.csv'
+pocket_feature_url = '/Users/allikeys/Documents/github/scaffold_hops/data/ligandComparisons.csv'
--- services/uniprot.py
+++ services/uniprot.py
@@ -20,3 +20,24 @@ def pdb2uniprot(pdb_ids):
     req = urllib.request.Request(url, data)
     mapping_file = urllib.request.urlopen(req)
     return mapping_file
+
+def pdb2uniprotAC(pdb_ids):
+    """ID exchange for mapping PDB IDs to Uniprot IDs
+    Input: pdb_ids (list)
+    Output: Returns file object with mappings
+    """
+    url = 'https://www.uniprot.org/uploadlists/'
+
+    params = {
+    'from': 'PDB_ID',
+    'to': 'ACC',
+    'format': 'tab',
+    'query': ' '.join(pdb_ids)
+    }
+
+    data = urllib.parse.urlencode(params)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    mapping_file = urllib.request.urlopen(req)
+    return mapping_file
+
