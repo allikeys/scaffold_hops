e00aedd2c313e660ad402a475953ff3d51764cdf
allikeys <37038507+allikeys@users.noreply.github.com>
2021-03-06 14:30:46 -0500

    Add files via upload

---
+++ ligandSimilarity/extract_smiles.py
@@ -0,0 +1,30 @@
+import sys
+import re
+import os
+import io
+from io import open 
+
+def main():
+
+	# Get list of all PDB ligands: SMILES     HET     name
+	all_ligands = open('all_smiles.txt', 'r')
+	all_ligands_INFO = all_ligands.readlines()
+
+	# Get list of Ligands in scPDB
+	ligand_list = open('ligand_list.txt', 'r')
+	ligands_HET = ligand_list.readlines()
+	for i in range(len(ligands_HET)):
+		ligands_HET[i] = ligands_HET[i].rstrip()
+
+	# New List of ligand SMILES
+	smiles = []
+
+	for i in range(len(all_ligands_INFO)):
+		line = all_ligands_INFO[i]
+		split_line = re.split(r'\s+', line)
+		if split_line[1] in ligands_HET:
+			print(split_line[0])
+
+
+if __name__ == "__main__":
+	main()
